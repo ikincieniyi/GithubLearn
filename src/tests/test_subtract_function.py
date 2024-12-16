4237b87632a0380cdd6b0b793fde87487c7331a7
ikincieniyi <zekeriyaorman1903@gmail.com>
2024-12-16 20:43:14 +0300

    test sub added

---
+++ src/tests/test_subtract_function.py
@@ -0,0 +1,14 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
+
+from src.main import subtract
+
+def test_subtract():
+    assert subtract(1, 2) == -1
+    assert subtract(1, 1) == 0
+    assert subtract(0, 0) == 0
+    assert subtract(-1, -1) == 0
+    assert subtract(-1, 1) == -2
+    assert subtract(1, -1) == 2
+    assert subtract(1, 0) == 1
